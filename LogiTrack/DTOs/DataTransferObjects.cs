6b9c7193db0fdb0fb42447297f463be76a69bb2c
Edison Kardirsson <ed.kardirsson@hotmail.com>
2025-08-06 13:50:37 +0200

    DTO created

---
+++ LogiTrack/DTOs/DataTransferObjects.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LogiTrack.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+    
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+public class LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
+
+public class AuthResponseDto
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public string? Token { get; set; }
+    public string? Email { get; set; }
+    public List<string> Roles { get; set; } = new();
+    public DateTime? Expiration { get; set; }
+}
